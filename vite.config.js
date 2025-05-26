964f28aa9324eb19d1b79a989e8aefa13db6ae34
songtomtom <celvincolvumn@gmail.com>
2025-05-26 14:39:42 +0900

    package.json 버전 업데이트 및 twgl.js alias 추가

--- vite.config.js
+++ vite.config.js
@@ -57,7 +57,9 @@ export default defineConfig(({ mode }) => ({
 
     resolve: {
         alias: {
-            '@': resolve(process.cwd(), './src')
+            '@': resolve(process.cwd(), './src'),
+            // twgl.js default export 문제 해결
+            'twgl.js': resolve(process.cwd(), 'node_modules/twgl.js/dist/4.x/twgl-full.js')
         }
     },
 
@@ -77,4 +79,3 @@ export default defineConfig(({ mode }) => ({
         exclude: ['raw-loader']
     }
 }));
-
